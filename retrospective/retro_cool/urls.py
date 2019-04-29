a92792024a2f505f7fdb6f8bd1d88991f6fdf59c
mszotek <mszotek@gmail.com>
2019-04-29 15:08:40 +0200

    Setup config

---
+++ retrospective/retro_cool/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from retro_cool import views
+
+urlpatterns = [
+    path('', views.main, name='retro_list'),
+    path('/add', views.add, name='retro_add'),
+    path('/view', views.view, name='retro_view')
+]
--- retrospective/retro_cool/views.py
+++ retrospective/retro_cool/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def main(request):
+    pass
+
+
+def add(request):
+    pass
+
+
+def view(request):
+    pass
