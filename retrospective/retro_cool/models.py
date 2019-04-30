6d6a6d55897c9bd988f005ec0ccf0381ae057989
Kamil Bednarczyk <kamil2.bednarczyk@gmail.com>
2019-04-30 15:04:46 +0200

    Implement Filling in retro board

--- retrospective/retro_cool/models.py
+++ retrospective/retro_cool/models.py
@@ -8,11 +8,17 @@ class Retrospective(models.Model):
     room = models.CharField(max_length = 20)
     date = models.DateField()
 
+    def __str__(self):
+        return self.room
+
 
 class KeepItem(models.Model):
 
     retrospective = models.ForeignKey(Retrospective, on_delete=models.CASCADE)
     text = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.text
 
 
 class ImproveItem(models.Model):
@@ -20,3 +26,5 @@ class ImproveItem(models.Model):
     retrospective = models.ForeignKey(Retrospective, on_delete=models.CASCADE)
     text = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.text
